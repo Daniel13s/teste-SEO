11c0457cc86d5f89d340e70a58568507c9a6ae26
Daniel Silva <dansilvac254@gmail.com>
2025-12-06 15:47:25 -0300

    update: add more keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "JC Piscinas | A sua construtora em Cajazeiras",
   description: "Procurando uma construtora? Pense em seu futuro com a melhor qualidade e garantia, venha para a melhor construtora de Cajazeiras.",
-  keywords: ["construtora em cajazeiras", "construtora cajazeiras", "jc piscinas", "construção cajazeiras", "melhor construtora cajazeiras", "construtora de qualidade"],
+  keywords: ["construtora em cajazeiras", "construtora em cajazeiras pb", "construtora cajazeiras", "construtora cajazeiras pb", "jc piscinas", "construção cajazeiras", "construção cajazeiras pb", "melhor construtora cajazeiras", "melhor construtora cajazeiras pb", "construtora de qualidade"],
   robots: {
     index: true,
     follow: true,
