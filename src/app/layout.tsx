8142c87f9115a6d3e63995c2a3712e108370c13d
Daniel Silva <dansilvac254@gmail.com>
2025-12-11 08:08:15 -0300

    feat: add search image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,14 @@ export const metadata: Metadata = {
   title: "JC Piscinas | A sua construtora em Cajazeiras",
   description: "Procurando uma construtora? Pense em seu futuro com a melhor qualidade e garantia, venha para a melhor construtora de Cajazeiras.",
   keywords: ["construtora em cajazeiras", "construtora em cajazeiras pb", "construtora cajazeiras", "construtora cajazeiras pb", "jc piscinas", "construção cajazeiras", "construção cajazeiras pb", "melhor construtora cajazeiras", "melhor construtora cajazeiras pb", "construtora de qualidade"],
+  other: {
+    "script:ld+json": JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "url": "https://teste-seo-flame.vercel.app",
+      "logo": "https://teste-seo-flame.vercel.app/logo.webp"
+    })
+  },
   robots: {
     index: true,
     follow: true,
